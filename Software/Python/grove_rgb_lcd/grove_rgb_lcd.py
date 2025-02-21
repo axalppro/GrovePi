0754adbba6f92413238ce21da90c66a3574589c9
axalppro <aurerit2000@hotmail.fr>
2025-02-21 11:57:30 +0100

    Fix I2C address in grove_rgb_lcd.py
    Fix I2C address for the RPI CM4

--- Software/Python/grove_rgb_lcd/grove_rgb_lcd.py
+++ Software/Python/grove_rgb_lcd/grove_rgb_lcd.py
@@ -22,17 +22,8 @@
 
 import time,sys
 
-if sys.platform == 'uwp':
-    import winrt_smbus as smbus
-    bus = smbus.SMBus(1)
-else:
-    import smbus
-    import RPi.GPIO as GPIO
-    rev = GPIO.RPI_REVISION
-    if rev == 2 or rev == 3:
-        bus = smbus.SMBus(1)
-    else:
-        bus = smbus.SMBus(0)
+# I2C address of the LCD
+bus = smbus.SMBus(1)
 
 # this device has two I2C addresses
 DISPLAY_RGB_ADDR = 0x62
